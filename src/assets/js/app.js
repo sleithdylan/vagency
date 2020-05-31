3bc30ab76fd1843bf7718d4f1fe6039a8e9aa2d8
Dylan Sleith <sleithdylan@gmail.com>
2020-05-31 22:57:39 +0100

    Add sponsors

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,10 +19,6 @@ var tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.from('.logo', { x: -50, opacity: 0 }, '-=.1');
 
 tl.from('.stagger', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
+  .from('.sponsors', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
   .to('h1 span', { clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=.8') // Swipe with Delay
   .from('.content-visual', { scaleX: 0.8, scaleY: 0.8, opacity: 0 }, '-=1'); // Fade Up with Delay
-
-// ScrollReveal
-// ScrollReveal().reveal('.content-services-text');
-// ScrollReveal().reveal('.content-services-desc');
-// ScrollReveal().reveal('.card', { delay: 400 });
