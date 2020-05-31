91d21f6c330c1f9bcd5cfa569b6daa64f5dbf326
Dylan Sleith <sleithdylan@gmail.com>
2020-05-31 01:47:59 +0100

    Add GSAP

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,3 +12,10 @@ open.addEventListener('click', () => {
 exit.addEventListener('click', () => {
   nav.classList.remove('open-nav');
 });
+
+// Create GSAP Instance
+var tl = gsap.timeline({ defaults: { duration: 1 } });
+
+tl.from('.stagger', { y: 50, opacity: 0, stagger: 0.7 }, '-=.4') // Fade Up
+  .to('h1 span', { clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=.8') // Swipe with Delay
+  .from('.content-visual', { scaleX: 0.8, scaleY: 0.8, opacity: 0 }, '-=1'); // Fade Up with Delay
