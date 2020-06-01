58be5bdcdb87f5735b5d0813e6a851a6b0a12264
Dylan Sleith <sleithdylan@gmail.com>
2020-06-01 15:19:19 +0100

    Add css naming conventions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,7 @@ const get = element => document.getElementById(element);
 
 let open = get('menu-btn');
 let nav = get('nav');
-let exit = get('exit-btn');
+let exit = get('nav__btn--exit');
 
 open.addEventListener('click', () => {
   nav.classList.add('open-nav');
@@ -19,6 +19,6 @@ var tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.from('.logo', { x: -50, opacity: 0 }, '-=.1');
 
 tl.from('.stagger', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
-  .from('.sponsors', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
+  .from('.hero__sponsors', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
   .to('h1 span', { clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=.8') // Swipe with Delay
-  .from('.content-visual', { scaleX: 0.8, scaleY: 0.8, opacity: 0 }, '-=1'); // Fade Up with Delay
+  .from('.hero__content-visual', { scaleX: 0.8, scaleY: 0.8, opacity: 0 }, '-=1'); // Fade Up with Delay
