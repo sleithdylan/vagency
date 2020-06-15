dfd78261cd44c735d15e3de70639295b5b510309
Dylan Sleith <sleithdylan@gmail.com>
2020-06-15 14:28:07 +0100

    Add stagger

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -27,7 +27,7 @@ var tl = gsap.timeline({ defaults: { duration: 1 } });
 
 tl.from('.logo', { x: -50, opacity: 0 }, '-=.1');
 
-// tl.from('.stagger', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
-//   .from('.hero__sponsors', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
-//   .to('h1 span', { clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=.8') // Swipe with Delay
-//   .from('.hero__content-visual', { scaleX: 0.8, scaleY: 0.8, opacity: 0 }, '-=1'); // Fade Up with Delay
+tl.from('.stagger', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
+  .from('.hero__sponsors', { y: 50, opacity: 0, stagger: 0.7 }, '-=.7') // Fade Up
+  .to('h1 span', { clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=.8') // Swipe with Delay
+  .from('.hero__content-visual', { scaleX: 0.8, scaleY: 0.8, opacity: 0 }, '-=1'); // Fade Up with Delay
